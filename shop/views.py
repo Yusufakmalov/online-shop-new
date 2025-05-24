f1078d53bac4c8fca05d6ebc9db6bf24bf8c15f3
Yusufakmalov <144015110+Yusufakmalov@users.noreply.github.com>
2025-05-24 22:52:52 +0500

    Update views.py

--- shop/views.py
+++ shop/views.py
@@ -27,6 +27,7 @@ def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
+    new_products = Post.objects.filter(available=True).order_by('-created')
     if category_slug:
         category = get_object_or_404(Category,
                                     slug=category_slug)
